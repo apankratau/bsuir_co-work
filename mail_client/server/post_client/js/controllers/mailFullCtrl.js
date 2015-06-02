c2bb0d4468530a42683b6f15ec14eef247bd8c06
Raisky34 <Raisky34@gmail.com>
2015-06-02 16:24:57 +0300

    Base routing

--- mail_client/server/post_client/js/controllers/mailFullCtrl.js
+++ mail_client/server/post_client/js/controllers/mailFullCtrl.js
@@ -4,3 +4,19 @@ var module = angular.module('userApp',['restangular','ui.router'])
 var guestCtrl = module.
 	controller('mailFullCtrl', function($scope, $log, Restangular) {	 
 	});
+
+	module.config(function($stateProvider, $urlRouterProvider) {
+		  
+		  // For any unmatched url, redirect to /state1
+		  $urlRouterProvider.otherwise("/mail");
+		   
+		  //Set up the states
+		  var mailFull = {
+		      name: 'mailFull',
+		      url: '/#/mailfull',
+		      templateUrl: 'views/mailFull.html',
+		      data: {}
+		  }
+		  $stateProvider
+		    .state(compose)
+	});
--- mail_client/server/post_client/js/controllers/mailListCtrl.js
+++ mail_client/server/post_client/js/controllers/mailListCtrl.js
@@ -4,3 +4,19 @@ var module = angular.module('userApp',['restangular','ui.router'])
 var guestCtrl = module.
 	controller('mailListCtrl', function($scope, $log, Restangular) {	 
 	});
+
+	module.config(function($stateProvider, $urlRouterProvider) {
+		  
+		  // For any unmatched url, redirect to /state1
+		  $urlRouterProvider.otherwise("/mail");
+		   
+		  //Set up the states
+		  var mailList = {
+		      name: 'mailList',
+		      url: '/#/maillist',
+		      templateUrl: 'views/mailList.html',
+		      data: {}
+		  }
+		  $stateProvider
+		    .state(compose)
+	});
